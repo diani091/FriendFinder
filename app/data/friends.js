c0cf14951287ca1172ace9e0cbaa4c94ac49c623
Diani <diana.mty@fabcafe.com>
2018-07-23 12:35:05 -0500

    Homework details

---
+++ app/data/friends.js
@@ -0,0 +1,70 @@
+var friendList = [
+    {
+      name: "Bojack",
+      profilePic: "https://www.indiewire.com/wp-content/uploads/2017/09/1351055-94035-zoomed-1.jpg?w=780",
+      scores: [
+        "3",
+        "1",
+        "1",
+        "5",
+        "3",
+        "1",
+        "3",
+        "2",
+        "3",
+        "5"
+      ]
+    },
+    {
+        name: "Mr. Meeseks",
+        profilePic: "https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F11d85b0466487d61a86010d4375fde12.300x300x1.jpg",
+        scores: [
+          "2",
+          "2",
+          "2",
+          "2",
+          "2",
+          "2",
+          "3",
+          "1",
+          "2",
+          "2"
+        ]
+      },
+
+      {
+        name: "Ramona Flowers",
+        profilePic: "https://cs4.pikabu.ru/post_img/big/2015/11/06/1/1446765556_1615117602.png",
+        scores: [
+          "5",
+          "2",
+          "5",
+          "1",
+          "1",
+          "5",
+          "2",
+          "4",
+          "5",
+          "5"
+        ]
+      },
+
+      {
+        name: "Batman",
+        profilePic: "https://i.pinimg.com/originals/94/0c/24/940c2410f772ba29f5631c35d0993cd0.jpg",
+        scores: [
+          "1",
+          "5",
+          "2",
+          "4",
+          "3",
+          "2",
+          "4",
+          "1",
+          "5",
+          "5"
+        ]
+      }
+  ]
+  
+  module.exports = friendList;
